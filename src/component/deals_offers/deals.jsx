1b86075f1ab85fe102186c0d60da3913fcaadf4f
Irakliko1 <samxaradzeirakli1@gmail.com>
2023-09-26 18:18:31 +0400

    API

--- src/component/deals_offers/deals.jsx
+++ src/component/deals_offers/deals.jsx
@@ -1,58 +1,52 @@
 import React from 'react'
 import './deals.css'
 import Timer from '../timer/timer'
-import Laptop from '../../assets/prodact/laptop.png'
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
 
 const Deals = () => {
+        const [offers, setOffers] = useState([]);
+
+  // const [selectedProductId, setSelectedProductId] = useState(null);
+
+  useEffect(() => {
+    // Make the API request when the component mounts
+    axios
+      .get('https://digitalamazonproject.azurewebsites.net/api/product/offers')
+      .then((response) => {
+        setOffers(response.data ) ; // Assuming the response contains an array of products
+
+      })
+      .catch((error) => {
+        console.error('API request error:', error);
+      });
+
+  }, []);       
+
   return (
     <div className='deals_container'>
       <div className='timer_container'>
           <Timer/>
       </div>
       <div className='prodact_container'>
+
+      {offers.map((offer) => {
+        return (
         <div className='sale_box'>
                 <Link to={'/productview'}>
-                <img src={Laptop} alt="Laptop" />
+                <img src={offer.image} alt="Laptop" />
                 </Link>
-                <span className='product_description'>Laptops</span>        
-                <span className='sale_percent'> -99%</span>
+                {/* <span className='product_description'>Laptops</span>         */}
+                <span className='sale_percent'> {offer.newPrice} $</span>
         </div>
-        <div className='sale_box'>
-                <img src={Laptop} alt="Laptop" />
-                <span className='product_description'>Laptops</span>        
-                <span className='sale_percent'> -99%</span>
-        </div>
-        <div className='sale_box'>
-                <img src={Laptop} alt="Laptop" />
-                <span className='product_description'>Laptops</span>        
-                <span className='sale_percent'> -99%</span>
-        </div>
-        <div className='sale_box'>
-                <img src={Laptop} alt="Laptop" />
-                <span className='product_description'>Laptops</span>        
-                <span className='sale_percent'> -99%</span>
-        </div>
-        <div className='sale_box'>
-                <img src={Laptop} alt="Laptop" />
-                <span className='product_description'>Laptops</span>        
-                <span className='sale_percent'> -99%</span>
-        </div>
-        <div className='sale_box'>
-                <img src={Laptop} alt="Laptop" />
-                <span className='product_description'>Laptops</span>        
-                <span className='sale_percent'> -99%</span>
-        </div>
-        <div className='sale_box'>
-                <img src={Laptop} alt="Laptop" />
-                <span className='product_description'>Laptops</span>        
-                <span className='sale_percent'> -99%</span>
-        </div>
-        <div className='sale_box'>
-                <img src={Laptop} alt="Laptop" />
-                <span className='product_description'>Laptops</span>        
-                <span className='sale_percent'> -99%</span>
-        </div>
+
+        )
+
+      })}
+        
       </div>
     </div>
   )
